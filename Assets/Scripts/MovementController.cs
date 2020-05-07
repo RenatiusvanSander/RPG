42d781f3b8daf5ef2056a078292f97e580008ac7
Remy Meier <remad@web.de>
2020-05-07 19:51:42 +0200

    master: corrected gravity mistake in project settings

--- Assets/Scripts/MovementController.cs
+++ Assets/Scripts/MovementController.cs
@@ -4,16 +4,37 @@ using UnityEngine;
 
 public class MovementController : MonoBehaviour
 {
+    /*
+    * Defines speed of movement.
+    */
+    public float movementSpeed = 3.0f;
+
+    // Holds the position of PlayerObject as 2D Vector.
+    Vector2 movement = new Vector2();
+
+    //
+    Rigidbody2D rb2D;
+
     // Start is called before the first frame update
     // Use this for initialization
-    void Start()
+    private void Start()
     {
-        
+        rb2D = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        // Keep this empty for now.
+    }
+
+    // ?
+    void FixedUpdate() {
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+
+        movement.Normalize();
+
+        rb2D.velocity = movement * movementSpeed;
     }
 }
