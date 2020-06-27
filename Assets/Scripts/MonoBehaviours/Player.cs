0fe9c6a6c3b287d78d7c588a4b227982a9e20b52
Remy Meier <remad@web.de>
2020-06-27 19:48:13 +0200

    chapter-05: Added AjustHitPoints

--- Assets/Scripts/MonoBehaviours/Player.cs
+++ Assets/Scripts/MonoBehaviours/Player.cs
@@ -21,9 +21,25 @@ public class Player : Character
 
             if(hitObject != null)
             {
-                print("it: " + hitObject.objectName);
+                print("Hit: " + hitObject.objectName);
+
+                switch(hitObject.itemType) {
+                    case Item.ItemType.COIN:
+                    break;
+                    case Item.ItemType.HEALTH:
+                    AdjustHitPoints(hitObject.quantity);
+                    break;
+                    default:
+                    break;
+                }
                 collison.gameObject.SetActive(false);
             }
         }
+    }
+
+    public void AdjustHitPoints(int amount) {
+        hitPoints = hitPoints + amount;
+        print("Adjusted hitpoints by: " + amount + ". New value: " + hitPoints);
+
     }
 }
